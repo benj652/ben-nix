007aa7475e67cbc1037434b3b94a1412e4a3db74
benj652 <benjaminjaffe8@gmail.com>
2026-02-15 17:43:00 -0500

    moved double hyprpaper

--- home.nix
+++ home.nix
@@ -58,49 +58,5 @@
     pcmanfm
   ];
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-
-    settings = {
-      "$mod" = "SUPER";
-
-      monitor = ",preferred,auto,1";
-
-      exec-once = [
-        "waybar"
-        "hyprpaper"
-      ];
-
-      input = {
-        kb_layout = "us";
-        follow_mouse = 1;
-      };
-
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-      };
-
-      decoration = {
-        rounding = 10;
-      };
-
-      bind = [
-        "$mod, RETURN, exec, kitty"
-        "$mod, Q, killactive"
-        "$mod, d, exec, wofi --show drun"
-        "$mod, h, movefocus, l"
-        "$mod, j, movefocus, d"
-        "$mod, k, movefocus, u"
-        "$mod, l, movefocus, r"
-
-        "$mod SHIFT, h, movewindow, l"
-        "$mod SHIFT, j, movewindow, d"
-        "$mod SHIFT, k, movewindow, u"
-        "$mod SHIFT, l, movewindow, r"
-      ];
-    };
-  };
 
 }
