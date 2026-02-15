b12ff18e1d74e8e44a08177bffe95898b885d5f4
benj652 <benjaminjaffe8@gmail.com>
2026-02-15 15:15:37 -0500

    Added flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Benj Nixos";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.benj-pc = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.benj = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
+}
