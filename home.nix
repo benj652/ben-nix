472e376f4bc45d13472be0502fbbc2666bc4f1fe
Tin Nguyen <nguyenvb.tin03@gmail.com>
2026-02-15 16:18:22 -0500

    home.packages: rm nix-search-tv

--- home.nix
+++ home.nix
@@ -26,15 +26,5 @@
     wofi
     nitch
     pcmanfm
-    (pkgs.writeShellApplication {
-      name = "ns";
-      runtimeInputs = with pkgs; [
-        fzf
-        (nix-search-tv.overrideAttrs {
-          env.GOEXPERIMENT = "jsonv2";
-        })
-      ];
-      text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
-    })
   ];
 }
