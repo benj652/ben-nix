540a5f1ee75dd4f9bec9f86da8eb233fdaa49781
Tin Nguyen <nguyenvb.tin03@gmail.com>
2026-02-15 17:01:44 -0500

    add nebula space colors

--- modules/theme.nix
+++ modules/theme.nix
@@ -18,6 +18,36 @@
     cyan = "#94e2d5";
   };
 
+  palette = {
+    bg_dim =      "#0d1117";
+    bg0 =         "#161c24";
+    bg1 =         "#1c242f";
+    bg2 =         "#232d39";
+    bg3 =         "#2b3745";
+    bg4 =         "#344151";
+    bg5 =         "#3e4c5d";
+    bg_visual =   "#352632";
+    bg_red =      "#3c2323";
+    bg_yellow =   "#353128";
+    bg_green =    "#223235";
+    bg_blue =     "#1e3041";
+    bg_purple =   "#2b283d";
+    fg =          "#b0c4de";
+    red =         "#9e3a3a";
+    orange =      "#b36d5d";
+    yellow =      "#a79a7a";
+    green =       "#5f8787";
+    aqua =        "#6699cc";
+    blue =        "#5d81ac";
+    purple =      "#897f9e";
+    grey0 =       "#5c6a72";
+    grey1 =       "#708090";
+    grey2 =       "#8da0b3";
+    statusline1 = "#5d81ac";
+    statusline2 = "#b0c4de";
+    statusline3 = "#9e3a3a";
+  };
+
   fonts = {
     monospace = "JetBrainsMono Nerd Font";
     ui = "Inter";
