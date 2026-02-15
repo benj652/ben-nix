eef072b84255d3659f2808df72dd2d7322092ec1
Tin Nguyen <nguyenvb.tin03@gmail.com>
2026-02-15 15:57:14 -0500

    add home.packages

--- home.nix
+++ home.nix
@@ -16,4 +16,25 @@
       fi
     '';
   };
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    wofi
+    nitch
+    pcmanfm
+    (pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        (nix-search-tv.overrideAttrs {
+          env.GOEXPERIMENT = "jsonv2";
+        })
+      ];
+      text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
+    })
+  ];
 }
