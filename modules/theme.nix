80630a9e03a4904bb9cce416ed119f25f4d3fb44
benj652 <benjaminjaffe8@gmail.com>
2026-02-15 16:36:26 -0500

    made defult theme.nix

---
+++ modules/theme.nix
@@ -0,0 +1,26 @@
+{
+  colors = {
+    background = "#1e1e2e";
+    backgroundAlt = "#181825";
+
+    foreground = "#cdd6f4";
+    foregroundMuted = "#a6adc8";
+
+    primary = "#89b4fa";
+    secondary = "#f38ba8";
+    accent = "#a6e3a1";
+
+    red = "#f38ba8";
+    green = "#a6e3a1";
+    yellow = "#f9e2af";
+    blue = "#89b4fa";
+    magenta = "#cba6f7";
+    cyan = "#94e2d5";
+  };
+
+  fonts = {
+    monospace = "JetBrainsMono Nerd Font";
+    ui = "Inter";
+  };
+}
+
